59a87219af3ba9e07874f159728aa6982d191cc3
Greg Lieske <grzesiu.lieske@gmail.com>
2016-04-21 12:13:30 +0200

    Splash - not working yet

--- BlueQueenWeather/MainActivity.cs
+++ BlueQueenWeather/MainActivity.cs
@@ -14,7 +14,7 @@ using System.Globalization;
 
 namespace BlueQueenWeather
 {
-    [Activity(Label = "BlueQueenWeather", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "BlueQueenWeather", MainLauncher = false, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         BlueQueen BQ;
