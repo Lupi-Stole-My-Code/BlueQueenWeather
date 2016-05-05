1029f5c72a30ce80c5694a900bf9bdc08d67ea99
Przemek Blokus <przemek.blokus@gmail.com>
2016-05-05 11:09:02 +0200

    Windows phone dodanie labelek i odswiezania. toasty trzeba dokonczyc

--- BlueQueen.Weather.WindowsPhone/MainPage.xaml.cs
+++ BlueQueen.Weather.WindowsPhone/MainPage.xaml.cs
@@ -12,6 +12,16 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using BlueQueen;
+using System.Net;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Net.Security;
+using System.Collections.Generic;
+using System.Globalization;
+using Windows.Phone.Notification;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -22,11 +32,25 @@ namespace BlueQueen.Weather.WindowsPhone
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        BlueQueenCore BQ;
+        List<WeatherInfo> WeatherData = new List<WeatherInfo>();
+        
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            
+
+            BQ = new BlueQueenCore(@"http://usafeapi.bluequeen.tk", "v1", "token");
+            CultureInfo culture = new CultureInfo("en-US");
+            WeatherData = BQ.getWeatherData(fromDate: DateTime.Now.ToString("d", culture));
+            //var test = WeatherData;
+            fillTextBlock();
         }
 
         /// <summary>
@@ -43,6 +67,52 @@ namespace BlueQueen.Weather.WindowsPhone
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+        }
+        public void fillTextBlock()
+        {
+            var data = WeatherData.FindLast(x => x.Value > -40);
+            textBlock.Text = data.Date.ToString() + " " + string.Format("{0}°C", data.Value.ToString()); ;
+        }
+    
+
+        private void refreshData(object sender, RoutedEventArgs e)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            WeatherData = BQ.getWeatherData(fromDate: DateTime.Now.ToString("d", culture));
+            fillTextBlock();
+            ShowToastNotification("abc");
+
+        }
+        private void ShowToastNotification(String message)
+        {
+            //http://stackoverflow.com/questions/23498187/display-local-toast-notification
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            // Set Text
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(message));
+
+            // Set image
+            // Images must be less than 200 KB in size and smaller than 1024 x 1024 pixels.
+            //XmlNodeList toastImageAttributes = toastXml.GetElementsByTagName("image");
+            //((XmlElement)toastImageAttributes[0]).SetAttribute("src", "ms-appx:///Images/logo-80px-80px.png");
+            //((XmlElement)toastImageAttributes[0]).SetAttribute("alt", "logo");
+
+            // toast duration
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "short");
+
+            // toast navigation
+            var toastNavigationUriString = "#/MainPage.xaml?param1=12345";
+            var toastElement = ((XmlElement)toastXml.SelectSingleNode("/toast"));
+            toastElement.SetAttribute("launch", toastNavigationUriString);
+
+            // Create the toast notification based on the XML content you've specified.
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            // Send your toast notification.
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
     }
 }
